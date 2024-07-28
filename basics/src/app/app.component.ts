08447c6c4a18eafa1efbbfe141244cfb0de05fb0
Rohit Chawhan <rohitchawhan555@gmail.com>
2024-07-28 23:11:37 +0530

    NgFor Directive

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -8,6 +8,11 @@ import { Component, signal } from "@angular/core";
 export class AppComponent {
   name = "john pollock!";
   imgUrl = "https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0";
+  images = [
+    "https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0",
+    "https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0",
+    "https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0",
+  ];
   currentDate = new Date();
   cost = 2000;
   temperature = 25.3;
